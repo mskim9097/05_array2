b568e11b1e7b0fcc69d5fbf1f51a00167cc68d06
MINSU KIM <mskim9097@gmail.com>
2022-12-14 15:11:25 +0900

    second commit

--- src/edu/kh/array2/practice/Array2Practice.java
+++ src/edu/kh/array2/practice/Array2Practice.java
@@ -44,5 +44,19 @@ public class Array2Practice {
 			System.out.println();
 		}
 	}
+	
+	public void practice4() { // 나중에 다시풀기
+		
+		int[][]arr = new int[4][4];
+		int sum = 0;
+		
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr[i].length; j++) {
+				if(i < 3 || j < 3) {
+					arr[i][j] = (int)(Math.random() * 10 + 1);
+				}
+			}
+		}
+	}
 
 }
